1a3e6e88f79b5914e49e7b58ff5a32fdfa216627
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-04-17 16:32:07 +0000

    nk_4_18

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.hosts << "8f4a1b71cdc24b66a9de96490dce7483.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
