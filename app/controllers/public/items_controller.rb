7d7076e9e8b75b86daab3b9f771e09d2bf855003
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-04-23 13:02:34 +0000

    4_23_1

---
+++ app/controllers/public/items_controller.rb
@@ -0,0 +1,12 @@
+class Public::ItemsController < ApplicationController
+  
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+ 
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -12,7 +11,7 @@ Rails.application.routes.draw do
 scope module: :public do
    root to: "homes#top"
    get 'homes/about' => "homes#about", as: 'about'
-
+   resources :items, only: [:show, :index]
    resource :customers, only: [:show]
    get 'customers/check' => "customers#check", as: 'check'
    get 'customers/information/edit' => "customers#edit", as: 'information_edit_customers'
@@ -20,11 +19,11 @@ scope module: :public do
    patch 'customers/withdraw' => "customers#withdraw", as: 'withdraw_customers'
   end
 
-   namespace :admin do
-    get 'homes/top'
-    resources :customers, only: [:index, :show, :edit]
-    resources :genres, only: [:index,:create, :edit, :update]
-    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+namespace :admin do
+   get 'homes/top'
+   resources :customers, only: [:index, :show, :edit]
+   resources :genres, only: [:index,:create, :edit, :update]
+   resources :items, only: [:new, :create, :index, :show, :edit, :update]
   end
 
 # root to: "homes#top"
