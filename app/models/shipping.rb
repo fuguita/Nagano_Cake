7c30280879595206adb7b87d53097cbf802d7918
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-05-12 18:00:06 +0000

    layout_order

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -3,7 +3,7 @@ class Shipping < ApplicationRecord
     belongs_to :customer
 
     def shipping_display
-      '〒' + postal_code + '' + address + '' + name
+      '〒'+postal_code+""+address+""+""+name
     end
 
 end
